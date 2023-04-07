7853a3a58eddaf0825a5e1dc8d63e5ca56e5be3b
Ali-Najaf <syedalinajafzaidi114@gmail.com>
2023-04-07 10:00:58 +0500

    I Changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1. Write a JavaScript program that accepts two numbers in two prompts and displays the larger one in the console.
 
-let num1 = +prompt("Enter 1st number")
-let num2 = +prompt("Enter 2nd number")
+let num1 = +prompt("Enter 1st number");
+let num2 = +prompt("Enter 2nd number");
 if(num1 > num2){
     console.log(num1);
 } else if(num2 > num1){
@@ -17,13 +17,13 @@ else{
 // Sample: -5
 // Output : The sign is -
 
-let num = +prompt("Enter the number")
+let num = +prompt("Enter the number");
 if(num > 0){
-    alert("The number is positive")
+    alert("The number is positive");
 }else if(num < 0){
-    alert("The number is nagative")
+    alert("The number is nagative");
 }else{
-    alert("The number is neither positive nor negative")
+    alert("The number is neither positive nor negative");
 }
 
 // 3. Write a JavaScript program that accepts five numbers in five prompts and displays the larger one in the console.
@@ -54,11 +54,11 @@ console.log(largestNum);
 
 for(i = 0; i <= 15; i++){
     if(i % 2 == 0){
-        document.write(i + " is even")
-        document.write('<br>')
+        document.write(i + " is even");
+        document.write('<br>');
     } else {
-        document.write(i + " is odd")
-        document.write('<br>')
+        document.write(i + " is odd");
+        document.write('<br>');
     }
 }
 
@@ -74,52 +74,52 @@ for(i = 0; i <= 15; i++){
 // <100      A
 
 
-let sub1 = +prompt("Enter subject 1's number")
-let sub2 = +prompt("Enter subject 2's number")
-let sub3 = +prompt("Enter subject 3's number")
-let sub4 = +prompt("Enter subject 4's number")
-let sub5 = +prompt("Enter subject 5's number")
-let sub6 = +prompt("Enter subject 6's number")
-let totalNum =+prompt("What are the total numbers?")
+let sub1 = +prompt("Enter subject 1's number");
+let sub2 = +prompt("Enter subject 2's number");
+let sub3 = +prompt("Enter subject 3's number");
+let sub4 = +prompt("Enter subject 4's number");
+let sub5 = +prompt("Enter subject 5's number");
+let sub6 = +prompt("Enter subject 6's number");
+let totalNum =+prompt("What are the total numbers?");
 
 let obtainedMarks = sub1 + sub2 + sub3 + sub4 + sub5 + sub6;
 
-let average = obtainedMar / totalMarks * 100;
+let average = obtainedMarks / totalMarks * 100;
 
 if (average <= 60) {
-    document.write("Your Grade is F and " + " Your average is " + average + "%")
+    document.write("Your Grade is F and " + " Your average is " + average + "%");
 }
 if (average <= 70 && average > 60) {
-    document.write("Your Grade is D and " + " Your average is " + average + "%")
+    document.write("Your Grade is D and " + " Your average is " + average + "%");
 }
 if (average <= 80 && average > 70) {
-    document.write("Your Grade is C and "  + " Your average is " + average + "%")
+    document.write("Your Grade is C and "  + " Your average is " + average + "%");
 }
 if (average <= 90 && average > 80) {
-   document.write("Your Grade is B and "  + " Your average is " + average + "%")
+   document.write("Your Grade is B and "  + " Your average is " + average + "%");
 }
 if (average <= 100 && average > 90) {
-    document.write("Your Grade is A and "  + " Your average is " + average + "%")
+    document.write("Your Grade is A and "  + " Your average is " + average + "%");
 }
 
 // 6. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 for (i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        document.write("FizzBuzz")
-        document.write('<br>')
+        document.write("FizzBuzz");
+        document.write('<br>');
     }
     else if (i % 3 === 0) {
-        document.write("Fizz")
-        document.write('<br>')
+        document.write("Fizz");
+        document.write('<br>');
     }
     else if (i % 5 === 0) {
-        document.write("Buzz")
-        document.write('<br>')
+        document.write("Buzz");
+        document.write('<br>');
     }
     else {
-        document.write(i)
-        document.write('<br>')
+        document.write(i);
+        document.write('<br>');
     }
 } 
 
@@ -132,22 +132,22 @@ for (i = 1; i <= 100; i++) {
 // * * * * *
 
 for (i = 1; i <= 1; i++) {
-    document.write("*")
-    document.write('<br>')
+    document.write("*");
+    document.write('<br>');
     for (i = 1; i <= 2; i++) {
-        document.write("*")
+        document.write("*");
     }
-    document.write('<br>')
+    document.write('<br>');
     for (i = 1; i <= 3; i++) {
-        document.write("*")
+        document.write("*");
     }
-    document.write('<br>')
+    document.write('<br>');
     for (i = 1; i <= 4; i++) {
-        document.write("*")
+        document.write("*");
     }
-    document.write('<br>')
+    document.write('<br>');
     for (i = 1; i <= 5; i++) {
-        document.write("*")
+        document.write("*");
     }
-    document.write('<br>')
+    document.write('<br>');
 } 
